da310711d37b2f67b086c29f5f0ed818e912af2a
Ji hyun Lee <21600555@handong.edu>
2018-02-07 17:03:59 +0900

    parthenon

--- Assets/ParthenonBuilder.cs
+++ Assets/ParthenonBuilder.cs
@@ -1,45 +1,107 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
+using System;
+using System.Linq;
 using UnityEngine;
 
 public class ParthenonBuilder : MonoBehaviour
 {
-
     public GameObject cubePrefab;
     public GameObject cylinderPrefab;
-    public float Floor_Width = 5.0f;
-    public float Floor_Depth = 10.0f;
-    public float Floor_Height = 0.25f;
-    public float Pillar_Radius = 0.2f;
-    public float Pillar_Height = 2.0f;
-    public float Pillar_Count_Width = 6.0f;
-    public float Pillar_Count_Depth = 10.0f;
-    public float Roof_Height = 1.0f;
+    public float floorWidth = 31.0f;
+    public float floorDepth = 69.5f;
+    public float floorHeight = 0.1f;
+    public float pillarRadius = 0.5f;
+    public float pillarHeight = 1.0f;
+    public float pillarCountWidth = 3;
+    public float pillarCountDepth = 6;
+    public float roofHeight = 1.0f;
+    public Material floorMaterial;
+    public Material pillarMaterial;
+    public Material roofMaterial;
 
-    public Material Floor_Material;
-    public Material Pillar_Material;
-    public Material Roof_Material;
+    float topFloorWidth;
+    float topFloorDepth;
+    float topFloorHeight;
 
-
-
-    [ContextMenu("Parthenon Build")]
-    void Parthenon_Build()
+    [ContextMenu("Build Now")]
+    void BuildNow()
     {
-        var floor1 = Instantiate(cubePrefab, transform);
-        floor1.transform.localPosition = new Vector3(0, 0, 0);
-        floor1.transform.localScale = new Vector3(Floor_Width, Floor_Height, Floor_Depth);
-
-        var floor2 = Instantiate(cubePrefab, transform);
-        floor2.transform.localPosition = new Vector3(0, Floor_Height, 0);
-        floor2.transform.localScale = new Vector3(Floor_Width * 0.95f, Floor_Height, Floor_Depth * 0.95f);
-
-        var floor3 = Instantiate(cubePrefab, transform);
-        floor3.transform.localPosition = new Vector3(0, Floor_Height * 2, 0);
-        floor3.transform.localScale = new Vector3(Floor_Width * 0.9025f, Floor_Height, Floor_Depth * 0.9025f);
+        DestroyAllChildren();
+        InstantiateFloor();
+        InstantiatePillars();
+        InstantiateRoof();
     }
 
-    [ContextMenu("Parthenon Pillar")]
-    void Parthenon_Pillar()
+    private void InstantiateRoof()
     {
+        var roof = Instantiate(cubePrefab, transform);
+        roof.transform.localScale = new Vector3(topFloorWidth, roofHeight, topFloorDepth);
+        roof.transform.Translate(Vector3.up * (topFloorHeight + pillarHeight));
+        roof.GetComponentInChildren<Renderer>().material = roofMaterial;
+    }
+
+    private void InstantiatePillars()
+    {
+        var pillarIntervalWidth = 2 * (topFloorWidth / 2 - pillarRadius) / (pillarCountWidth - 1);
+        InstantiatePillarWidthDirection(pillarIntervalWidth, -topFloorDepth / 2 + pillarRadius);
+        InstantiatePillarWidthDirection(pillarIntervalWidth, +topFloorDepth / 2 - pillarRadius);
+
+        var pillarIntervalDepth = 2 * (topFloorDepth / 2 - pillarRadius) / (pillarCountDepth - 1);
+        InstantiatePillarDepthDirection(pillarIntervalDepth, -topFloorWidth / 2 + pillarRadius);
+        InstantiatePillarDepthDirection(pillarIntervalDepth, +topFloorWidth / 2 - pillarRadius);
+    }
+
+    private void InstantiatePillarWidthDirection(float pillarIntervalWidth, float z)
+    {
+        for (int i = 0; i < pillarCountWidth; i++)
+        {
+            var pillar = Instantiate(cylinderPrefab, transform);
+            pillar.transform.localScale = new Vector3(pillarRadius / 0.5f, pillarHeight, pillarRadius / 0.5f);
+            pillar.transform.localPosition = new Vector3(-topFloorWidth / 2 + pillarRadius + pillarIntervalWidth * i, topFloorHeight, z);
+            pillar.GetComponentInChildren<Renderer>().material = pillarMaterial;
+        }
+    }
+
+    private void InstantiatePillarDepthDirection(float pillarIntervalDepth, float x)
+    {
+        for (int i = 1; i < pillarCountDepth - 1; i++)
+        {
+            var pillar = Instantiate(cylinderPrefab, transform);
+            pillar.transform.localScale = new Vector3(pillarRadius / 0.5f, pillarHeight, pillarRadius / 0.5f);
+            pillar.transform.localPosition = new Vector3(x, topFloorHeight, -topFloorDepth / 2 + pillarRadius + pillarIntervalDepth * i);
+            pillar.GetComponentInChildren<Renderer>().material = pillarMaterial;
+        }
+    }
+
+    private void InstantiateFloor()
+    {
+        var floor1 = Instantiate(cubePrefab, transform);
+        floor1.transform.localScale = new Vector3(floorWidth, floorHeight, floorDepth);
+        floor1.GetComponentInChildren<Renderer>().material = floorMaterial;
+
+        var unitSizeDecrement = floorWidth / 20;
+
+        var floor2 = Instantiate(cubePrefab, transform);
+        floor2.transform.localScale = new Vector3(floorWidth - unitSizeDecrement, floorHeight, floorDepth - unitSizeDecrement);
+        floor2.transform.Translate(Vector3.up * floorHeight);
+        floor2.GetComponentInChildren<Renderer>().material = floorMaterial;
+
+        var floor3 = Instantiate(cubePrefab, transform);
+        floor3.transform.localScale = new Vector3(floorWidth - unitSizeDecrement * 2, floorHeight, floorDepth - unitSizeDecrement * 2);
+        floor3.transform.Translate(Vector3.up * floorHeight * 2);
+        floor3.GetComponentInChildren<Renderer>().material = floorMaterial;
+
+        topFloorWidth = floor3.transform.localScale.x;
+        topFloorHeight = floorHeight * 3;
+        topFloorDepth = floor3.transform.localScale.z;
+    }
+
+    private void DestroyAllChildren()
+    {
+        foreach (Transform child in transform.Cast<Transform>().ToArray())
+        {
+            DestroyImmediate(child.gameObject);
+        }
+        Debug.Assert(transform.childCount == 0);
     }
 }
